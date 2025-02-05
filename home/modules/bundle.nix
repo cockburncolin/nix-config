a76e740cf381e3bd967273fa455441d9dae8419c
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-02-04 18:23:47 -0800

    mpv update

--- home/modules/bundle.nix
+++ home/modules/bundle.nix
@@ -3,6 +3,7 @@
     ./browsers/browsers.nix
     ./editors/editors.nix
     ./git/git.nix
+    ./media/media.nix
     ./shells/shells.nix
     ./terminals/terminals.nix
     ./wm/wm.nix
--- home/modules/media/mpv.nix
+++ home/modules/media/mpv.nix
@@ -26,7 +26,6 @@ in {
         profile = "gpu-hq";
         force-window = true;
         ytdl-format = "bestvideo+bestaudio";
-        cache-default = 4000000;
       };
     };
     programs.yt-dlp = {
