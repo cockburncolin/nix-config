254beda6af40781f5e339a6eb0109307b2f3380a
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-03-17 16:34:44 -0700

    update fish settings

--- home/modules/shells/fish.nix
+++ home/modules/shells/fish.nix
@@ -25,19 +25,34 @@ in
   };
 
   config = lib.mkIf config."${moduleBase}"."${moduleName}".enable {
-    programs.fish = {
-      enable = true;
-      plugins = [
-        {
-          name = "hydro";
-          src = pkgs.fetchFromGitHub {
-            owner = "jorgebucaran";
-            repo = "hydro";
-            rev = "75ab7168a35358b3d08eeefad4ff0dd306bd80d4";
-            sha256 = "0cd6fa6g0jdjdsxgy5qja2jx180s26v5hcs71npj5zim9sqvi2j1";
-          };
-        }
-      ];
+    programs = {
+      fish = {
+        enable = true;
+        loginShellInit = "tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes";
+        functions = {
+          fish_greeting = "";
+        };
+        plugins = [
+          {
+            name = "tide";
+            src = pkgs.fetchFromGitHub {
+              owner = "IlanCosman";
+              repo = "tide";
+              rev = "44c521ab292f0eb659a9e2e1b6f83f5f0595fcbd";
+              sha256 = "05svj1c6qz1bx7q3vyii7cnls0ibbbvd7dqj39c6crnw1kar967k";
+            };
+          }
+          {
+            name = "fzf.fish";
+            src = pkgs.fetchFromGitHub {
+              owner = "PatrickF1";
+              repo = "fzf.fish";
+              rev = "8920367cf85eee5218cc25a11e209d46e2591e7a";
+              hash = "sha256-T8KYLA/r/gOKvAivKRoeqIwE2pINlxFQtZJHpOy9GMM=";
+            };
+          }
+        ];
+      };
     };
   };
 }
--- home/modules/shells/shells.nix
+++ home/modules/shells/shells.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -15,10 +16,19 @@ in
     (import ./fish.nix argSet)
   ];
 
+  # Universial shell settings
   config = {
     # Shell aliases for shells
     home.shellAliases = {
       hms = "home-manager switch --flake $HOME/.config/nix#desktop -b backup";
     };
+
+    # Other shell utils
+    home.packages = with pkgs; [
+      bat
+      fd
+      fzf
+      thefuck
+    ];
   };
 }
