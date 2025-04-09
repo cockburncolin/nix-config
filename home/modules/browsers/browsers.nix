1804e0993fe8b9320fed10d5a5378ed8f18839c8
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-04-08 20:47:52 -0700

    Add Vivaldi browser

--- home/modules/browsers/browsers.nix
+++ home/modules/browsers/browsers.nix
@@ -9,5 +9,6 @@ in
   imports = [
     # (import ./firefox.nix argSet)
     (import ./librewolf.nix argSet)
+    (import ./vivaldi.nix argSet)
   ];
 }
---
+++ home/modules/browsers/vivaldi.nix
@@ -0,0 +1,30 @@
+# Takes moduleBase from importing file
+{
+  moduleBase ? "misc",
+}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  moduleName = "vivaldi";
+in
+{
+  imports = [ ];
+
+  options = {
+    "${moduleBase}"."${moduleName}" = {
+      enable = lib.mkOption {
+        default = true;
+        description = "enable ${moduleName}";
+        type = lib.types.bool;
+      };
+    };
+  };
+
+  config = lib.mkIf config."${moduleBase}"."${moduleName}".enable {
+    home.packages = with pkgs; [ vivaldi-browser ];
+  };
+}
