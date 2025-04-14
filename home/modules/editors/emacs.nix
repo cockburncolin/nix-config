1d8fd4e5d4914d9c1b68fa32e44c8a43d11b493c
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-04-13 18:46:58 -0700

    Add yassnippet.

--- home/modules/editors/emacs.nix
+++ home/modules/editors/emacs.nix
@@ -6,11 +6,12 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
 }:
 let
   moduleName = "emacs";
-  emacsDir = config.lib.file.mkOutOfStoreSymlink "/home/colin/.config/nix/home/modules/editors/emacs.d";
+  emacsPkg = pkgs.emacs-gtk;
 in
 {
   imports = [ ];
@@ -26,24 +27,33 @@ in
   };
 
   config = lib.mkIf config."${moduleBase}"."${moduleName}".enable {
-    xdg.configFile."emacs" = {
-      source = emacsDir;
-    };
+    xdg.configFile."emacs" =
+      let
+        srcpath = "${config.xdg.configHome}/nix/home/modules/editors/emacs.d";
+      in
+      {
+        source = config.lib.file.mkOutOfStoreSymlink srcpath;
+      };
 
     programs.emacs = {
       enable = true;
-      package = pkgs.emacs-gtk;
+      # Allows deletion of standalone emacs desktop file,
+      # reduces clutter in app launchers since I want to use client only
+      package = pkgs.symlinkJoin {
+        meta = emacsPkg.meta or "";
+        name = "emacs-without-desktop";
+        passthrough = emacsPkg.passthrough or "";
+        paths = [ emacsPkg ];
+        postBuild = ''
+          rm -f $out/share/applications/emacs.desktop
+          sed -i 's/^\(Exec=\).*$/\1emacsclient -r/g' $out/share/applications/emacsclient.desktop
+        '';
+      };
     };
 
     services.emacs = {
       enable = true;
+      defaultEditor = true;
     };
-    # Required for vterm
-    #   home.packages = with pkgs; [
-    #     cmake
-    #     gcc
-    #     gnumake
-    #     libtool
-    #   ];
   };
 }
