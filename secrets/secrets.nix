5a1c74579d58aa6080651aecacc34d3a71a9da6f
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-05-02 19:42:56 -0700

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   caeser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxzifYk38m22wJUWBXcpxQxCMD15e/K2l1kGdimHN4B";
   brutus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5alTXWq3csEW3JcQrggBwIRPlrYhtrYoCnmSXA9svA";
-  pliny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2EheUzT2zbCJ+r3HoCDCz6jso/lB9FCOW1R8XuLN8c nixos@iso";
+  pliny =  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8f0upV9tKUzknc9uJmzDyqH20bFWe0mcAqkGg/YYgF";
 in {
   "userpw.age".publicKeys = [
     brutus
